cedf53407fd23d61fd11715cb2714b2a8beeb5e5
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2024-01-26 10:47:39 +0700

    factory and seeder

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -4,20 +4,23 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
 class BookFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'title' => fake()->sentence(3),
-            'author' => fake()->name(),
+            'author' => fake()->name,
             'created_at' => fake()->dateTimeBetween('-2 years'),
-            'updated at' => fake()->dateTimeBetween('created at', 'now'),
+            'updated_at' => fake()->dateTimeBetween('created_at', 'now')
         ];
     }
 }
